710f11859d72daf6b0232cd4a608311272d191a2
ElderGr <gbielrs@gmail.com>
2022-07-28 14:42:03 -0300

    Preparando revisão de código

--- src/schemas/authSchema.ts
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-
-import { CreateUserData } from "../controllers/authController.js";
-
-export const authSchema = Joi.object<CreateUserData>({
-  email: Joi.string().email().required(),
-  password: Joi.string().min(10).required()
-});
